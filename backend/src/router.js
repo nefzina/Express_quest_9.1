5d435d68fb740a3052381383b03b4513b5107a8b
nefzina <nefzi.amani@gmail.com>
2023-07-03 10:44:44 +0200

    upload files with postman

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,26 @@
 const express = require("express");
-
+const multer = require("multer");
+const fs = require("fs");
+const { v4: uuidv4} = require("uuid")
 const router = express.Router();
+
+// On définit la destination de stockage de nos fichiers
+const upload = multer({ dest: "./public/uploads/" });
+
+// route POST pour recevoir un fichier dont le nom est "avatar"
+router.post("/avatar", upload.single("image"), (req, res) => {
+  const { originalname } = req.file; // On récupère le nom du fichier
+  const { filename } = req.file; // On récupère le nom du fichier
+
+  fs.rename(
+    `./public/uploads/${filename}`,
+    `./public/uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 
 const itemControllers = require("./controllers/itemControllers");
 
